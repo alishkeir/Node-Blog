8ae3b5157f8838c62c00ed3d0ad77d0af797a92c
alishkeir <alishkeir_@hotmail.com>
2023-09-22 01:26:30 +0300

    Finished Dashboard and Admin Panel

--- app.js
+++ app.js
@@ -2,28 +2,49 @@ require('dotenv').config();
 
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 
 const connectDB = require('./server/config/db');
+const { isActiveRoute } = require('./server/helpers/routeHelpers');
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-// connect to db
+// Connect to DB
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
 
-app.use(express.static('public')); // set public folder for public assets
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+    }),
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) ) }
+  })
+);
 
-//Templating Engine
+app.use(express.static('public'));
+
+// Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
-app.set('view engine', 'ejs'); // change the view engine to ejs (template engine)
+app.set('view engine', 'ejs');
+
+app.locals.isActiveRoute = isActiveRoute;
 
 app.use('/', require('./server/routes/main'));
 app.use('/', require('./server/routes/admin'));
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`App listening on port ${PORT}`);
 });
